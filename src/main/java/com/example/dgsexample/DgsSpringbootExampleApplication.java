456c1201d529dd49a9fbb8f8d153f2c66612f40b
MikeThomsenBAH <thomsen_michael@bah.com>
2023-04-25 18:20:12 -0400

    Added project skeleton.

---
+++ src/main/java/com/example/dgsexample/DgsSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsSpringbootExampleApplication.class, args);
+	}
+
+}
